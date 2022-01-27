5eb17e70e593885409c673f5d47a36e47a7dfbd5
RAJENDER SINGH NEGI <RAJENDER SINGH NEGI@DESKTOP-M9KU0VI>
2022-01-27 15:01:25 +0530

    Add project files.

---
+++ EmployeeManagementSystem/Models/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Models
+{
+    public class Employee
+    {
+        [Key]
+
+        [Required(ErrorMessage = "Required")]
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "Required")]
+        public String FName { get; set; }
+        
+        [Required(ErrorMessage = "Required")]
+        public String LName { get; set; }
+        
+        [Required(ErrorMessage = "Required")]
+        public String Email { get; set; }
+        
+        [Required(ErrorMessage = "Required")]
+        public String Password { get; set; }
+        
+        [Required(ErrorMessage = "Required")]
+        public String Mobile { get; set; }
+        
+        [Required(ErrorMessage = "Required")]
+        public String Gender { get; set; }
+        public String Experience { get; set; }
+        
+        [NotMapped]
+        public String Department { get; set; }
+    }
+}
